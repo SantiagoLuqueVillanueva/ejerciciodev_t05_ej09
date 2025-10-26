9c47a022481e177d28b38d321af70b8a29a7e080
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-26 20:13:47 +0100

    A medias

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Este programa cuenta los digitos de un número.");
+        int num = Integer.parseInt(System.console().readLine("Introduzca un número entero: "));
     }
 }
